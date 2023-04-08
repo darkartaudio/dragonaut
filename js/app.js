e25648887c6581b200d28e8645eaef3be0553d8b
darkartaudio <mikey@mikeyallred.com>
2023-04-07 19:15:48 -0500

    feat: movement checks for map dimensions and presence of barriers

--- js/app.js
+++ js/app.js
@@ -1,21 +1,26 @@
 // =================================================================================
 // GLOBAL DOM / VARIABLES
 // =================================================================================
+const gridSize = 32; // number of pixels in each map square, also corresponds to size of sprites (32 x 32) I'm using
+
 const game = document.querySelector('#game');
 const story = document.querySelector('#story');
 const choices = document.querySelector('#choices');
 const setupForm = document.querySelector('#setup-form');
-const gameStatus = document.querySelector('#game-status');
 
 const ctx = game.getContext('2d');
+// set up game status div, which will be displayed when game starts
+const gameStatus = document.createElement('div');
+gameStatus.setAttribute('id', 'game-status');
+gameStatus.textContent = 'hello!';
 
 let character;
 let runGame;
 
 // =================================================================================
 // MAP LEGEND
-// G = green dragon, W = white dragon, R = red dragon, B = black dragon
-// g = green book, w = white book, r = red book, b = black book
+// G = green dragon, W = white dragon, R = red dragon, Y = yellow dragon
+// g = green book, w = white book, r = red book, y = yellow book
 // C = character
 // 0 = wall tile
 // - = floor tile
@@ -34,9 +39,9 @@ let map = [
     ['-', '-', '-'], // 09
     ['-', 'r', '-'], // 10
     ['-', '-', '-'], // 11
-    ['0', 'B', '0'], // 12
+    ['0', 'Y', '0'], // 12
     ['-', '-', '-'], // 13
-    ['-', 'b', '-'], // 14
+    ['-', 'y', '-'], // 14
     ['-', '-', '-'], // 15
     ['0', 'C', '0']  // 16
 ];
@@ -50,8 +55,8 @@ wallTile.src = './img/stone-wall.png';
 const floorTile = document.createElement('img');
 floorTile.src = './img/stone-floor.png';
 
-const blackBook = document.createElement('img');
-blackBook.src = './img/book-black.png';
+const yellowBook = document.createElement('img');
+yellowBook.src = './img/book-yellow.png';
 
 const redBook = document.createElement('img');
 redBook.src = './img/book-red.png';
@@ -62,8 +67,8 @@ whiteBook.src = './img/book-white.png';
 const greenBook = document.createElement('img');
 greenBook.src = './img/book-green.png';
 
-const blackDragon = document.createElement('img');
-blackDragon.src = './img/dragon-black.png';
+const yellowDragon = document.createElement('img');
+yellowDragon.src = './img/dragon-yellow.png';
 
 const redDragon = document.createElement('img');
 redDragon.src = './img/dragon-red.png';
@@ -96,19 +101,20 @@ function removeMovementHandler() {
 setupForm.addEventListener('submit', (e) => {
     e.preventDefault();
     
+    // create new character with name and class as chosen by user, then remove
+    // setup form and display game status instead
     charName = setupForm.elements['character-name'].value;
     charClass = setupForm.elements['character-class'].value;
+    character = new Character(charName, charClass);
     setupForm.remove();
+    choices.append(gameStatus);
 
     
-    character = new Character(charName, charClass);
-    gameStatus.style.display = 'block';
-    
-    // TODO: setup map, add items and enemies
+    // TODO: add items and enemies
     
     runGame = setInterval(gameLoop, 60);
-    console.log(character);
-    // removeMovementHandler();
+
+    removeMovementHandler();
     document.addEventListener('keydown', movementHandler);
 });
 
@@ -129,29 +135,29 @@ class Character {
         this.class = charClass;
         this.health = 10 + Math.floor(Math.random() * 6); // start with 10 plus 0-5 hit points
         this.attackTypes = [];
-        this.height = 32;
-        this.width = 32;
-        this.x = 1 * 32;
-        this.y = 16 * 32;
-        this.imgURL = '';
-   
+        this.height = gridSize;
+        this.width = gridSize;
+
+        // starting map position
+        this.x = 1;
+        this.y = 16;
+
+        // set character icon image to match choice of class
+        this.img = document.createElement('img');
         switch (charClass) {
             case 'warrior':
-                this.imgURL = './img/warrior.png';
+                this.img.setAttribute('src', './img/warrior.png');
                 break;
             case 'wizard':
-                this.imgURL = "./img/wizard.png";
+                this.img.setAttribute('src', './img/wizard.png');
                 break;
             case 'ranger':
-                this.imgURL = "./img/ranger.png";
+                this.img.setAttribute('src', './img/ranger.png');
                 break;
         }
 
-        this.img = document.createElement('img');
-        this.img.setAttribute('src', this.imgURL);
-
         this.render = function () {
-            ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.img, this.x * gridSize, this.y * gridSize, this.width, this.height);
         }
     }
 
@@ -182,28 +188,47 @@ class Item {
 // KEYBOARD LOGIC
 // =================================================================================
 function movementHandler(e) {
-    // TODO: handle game boundaries
+    let targetX;
+    let targetY;
+
+    // set target coordinates based on which key
     switch (e.key) {
-        case 'w':
-        // case 'ArrowUp':
-            character.y - 32 >= 0 ? (character.y -= 32) : null;
+        case 'w': // up
+            targetX = character.x;
+            targetY = character.y - 1;
             break;
         
-        case 's':
-        // case 'ArrowDown':
-            character.y + 32 <= game.height - character.height ? (character.y += 32) : null;
+        case 's': // down
+            targetX = character.x;
+            targetY = character.y + 1;
             break;
 
-        case 'a':
-        // case 'ArrowLeft':
-            character.x - 32 >= 0 ? (character.x -= 32) : null;
+        case 'a': // left
+            targetX = character.x - 1;
+            targetY = character.y;
             break;
         
-        case 'd':
-        // case 'ArrowRight':
-            character.x + 32 <= game.width - character.width ? (character.x += 32) : null;
+        case 'd': // right
+            targetX = character.x + 1,
+            targetY = character.y;
             break;
     }
+
+    if (
+        targetX < 0 // trying to leave left edge of map
+        || targetX > map[0].length - 1 // right
+        || targetY > map.length - 1 // bottom
+        || targetY < 0 // top
+        || map[targetY][targetX] === '0' // barrier
+    ) {
+        return false;
+    } else {
+        character.x = targetX;
+        character.y = targetY;
+    }
+
+    // TODO: for each Dragon/Item, check for collision
+    checkForCollisions();
 }
 
 // TODO: disable movement while in combat
@@ -243,9 +268,6 @@ function gameLoop() {
     // TODO: for each Dragon/Item, render if alive
     renderDragons();
     renderItems();
-    
-    // TODO: for each Dragon/Item, check for collision
-    checkForCollisions();
 }
 
 // =================================================================================
